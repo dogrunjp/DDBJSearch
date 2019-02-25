ed6d46f4f0a0dc97d564293d65de3f69d9120619
oec <oec@dogrun.jp>
2019-02-25 15:59:03 +0900

    add config file

--- js/config.js
+++ js/config.js
@@ -1,7 +1,4 @@
 var conf = {
-    //api_search_base_url : "http://13.113.107.7/api/search?",
-    //api_details_base_url : "http://13.113.107.7/api/details?db=",
-    //api_base : "http://13.113.107.7/api",
-    //api_base: "http:http://52.193.211.138/api"
-    api_base : "http://localhost:8080/api",
+    api_base : "http://13.113.107.7/api",
+    //api_base : "http://localhost:8080/api",
 }
