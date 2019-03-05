0405e2040a59e98134b094c48088aad17b648a22
oec <oec@dogrun.jp>
2019-03-05 10:53:41 +0900

    edit configureation file

--- js/config.js
+++ js/config.js
@@ -1,8 +1,4 @@
 var conf = {
-    api_search_base_url : "http://localhost:8080/api/search?",
-    api_details_base_url : "http://localhost:8080/api/details?db=",
-    //api_search_base_url: "http://52.193.211.138/api/search?",
-    //api_details_base_url : "http://52.193.211.138/api/details?db=",
-    api_update_url: "http://52.193.211.138/api",
-    api_chart_url: "http://52.193.211.138/api"
+    api_base : "http://13.113.107.7/api",
+    //api_base : "http://localhost:8080/api",
 }
